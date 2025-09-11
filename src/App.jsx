e24c00bddde5e05ba924d2983757cfeb08da3293
GiapThe2002 <giapthuy2002@gmail.com>
2025-09-11 14:57:44 +0700

    create navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { ThemeProvider } from "./context/ThemeContext";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <ThemeProvider>
-      <div className="text-red-500">App</div>;
+      <div className="">
+        <Navbar />
+      </div>
     </ThemeProvider>
   );
 };
--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -3,27 +3,25 @@ import { createContext, useContext, useEffect, useState } from "react";
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [isDarkMode, toogleDarkMode] = useState(
-    localStorage.getItem("theme") || "light"
-  );
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
 
   useEffect(() => {
     const root = window.document.documentElement;
-    if (isDarkMode === "dark") {
+    if (theme === "dark") {
       root.classList.add("dark");
     } else {
       root.classList.remove("dark");
     }
-    localStorage.setItem("theme", isDarkMode);
-  }, [isDarkMode]);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
 
   return (
     <ThemeContext.Provider
-      value={{ isDarkMode: isDarkMode === "dark", toogleDarkMode }}
+      value={{ isDarkMode: theme === "dark", toogleDarkMode: setTheme }}
     >
       {children}
     </ThemeContext.Provider>
   );
 };
 
-export const useTheme = () => useContext(ThemeContext);
+export const UseTheme = () => useContext(ThemeContext);
