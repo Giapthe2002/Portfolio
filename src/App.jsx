edb44c1d27edbbdf338e028477d2923683031f97
GiapThe2002 <giapthuy2002@gmail.com>
2025-09-11 11:05:17 +0700

    create theme context darkmode

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { ThemeProvider } from "./context/ThemeContext";
 
 const App = () => {
-  return <div className="text-red-500">App</div>;
+  return (
+    <ThemeProvider>
+      <div className="text-red-500">App</div>;
+    </ThemeProvider>
+  );
 };
 
 export default App;
--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
-const ThemeContext = () => {
-  return <div>ThemeContext</div>;
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [isDarkMode, toogleDarkMode] = useState(
+    localStorage.getItem("theme") || "light"
+  );
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    if (isDarkMode === "dark") {
+      root.classList.add("dark");
+    } else {
+      root.classList.remove("dark");
+    }
+    localStorage.setItem("theme", isDarkMode);
+  }, [isDarkMode]);
+
+  return (
+    <ThemeContext.Provider
+      value={{ isDarkMode: isDarkMode === "dark", toogleDarkMode }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  );
 };
 
-export default ThemeContext;
+export const useTheme = () => useContext(ThemeContext);
